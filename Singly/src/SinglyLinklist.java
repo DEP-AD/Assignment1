f03b215d1386aed02e1bb573948e47a407417a5e
Aruni Dissanayake <aruni.saku@gmail.com>
2021-05-10 00:06:19 +0530

    method implement

--- Singly/src/SinglyLinklist.java
+++ Singly/src/SinglyLinklist.java
@@ -1,5 +1,42 @@
 public class SinglyLinklist {
+    public void add(int number){
+
+    }
+
+    public void add(int index,int number){
+
+    }
+
+    public void remove(int index){
+
+    }
+
+    public int get(int index){
+        return 0;
+    }
+
+    public void print(){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean contains(int index){
+        return false;
+    }
+
+    public boolean empty(){
+        return false;
+    }
+
     public static void main(String[] args) {
+        SinglyLinklist sl = new SinglyLinklist();
 
     }
 
--- Singly/src/linklist/Node.java
+++ Singly/src/linklist/Node.java
@@ -1,5 +1,12 @@
 package linklist;
 
 public class Node {
+        private int data;
+        private Node next;
 
+        public Node (int data){
+            this.data=data;
+            next=null;
+
+        }
 }
